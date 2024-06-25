84a7d9b295e8b6b7d66c7d6afaac3b657dbfdd50
Harriet124 <150043283+Harriet124@users.noreply.github.com>
2024-06-25 14:40:36 +0100

    initial commit

---
+++ .history/cypress/e2e/spec.cy_20240625143127.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://amalitech-workspace.slack.com/archives/D074KBVQRRBhttps://example.cypress.io')
+  })
+})
---
+++ .history/cypress/e2e/spec.cy_20240625143323.js
@@ -0,0 +1,6 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://practice-automation.com/')
+    cy.get('.wp-container-core-buttons-is-layout-8 > .wp-block-button > .wp-block-button__link').cl
+  })
+})
